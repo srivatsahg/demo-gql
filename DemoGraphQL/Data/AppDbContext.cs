f44c456aca57b7299f9839f0f69d7b5dcfc38507
Srivatsa Haridas <srivatsahg@gmail.com>
2021-11-29 20:46:01 +0530

    Adding initial set of files for GraphQL project

---
+++ DemoGraphQL/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DemoGraphQL.Models;
+
+namespace DemoGraphQL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
---
+++ DemoGraphQL/Models/Platform.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoGraphQL.Models
+{
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string LicenseKey { get; set; }
+    }
+}
